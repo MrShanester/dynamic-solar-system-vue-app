08ca38dee071430a68febffbcde9c21ced6fcfe3
Shane Nicholson <shanescottnicholson@gmail.com>
2021-11-19 11:53:45 -0800

    Adds signup page and action

---
+++ src/views/Signup.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="signup">
+    <form v-on:submit.prevent="submit()">
+      <h1>Signup</h1>
+      <ul>
+        <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+      </ul>
+      <div>
+        <label>Username:</label>
+        <p></p>
+        <input type="text" v-model="newUserParams.username" />
+      </div>
+      <p></p>
+      <div>
+        <label>Email:</label>
+        <p></p>
+        <input type="email" v-model="newUserParams.email" />
+      </div>
+      <p></p>
+      <div>
+        <label>Password:</label>
+        <p></p>
+        <input type="password" v-model="newUserParams.password" />
+      </div>
+      <p></p>
+      <div>
+        <label>Password confirmation:</label>
+        <p></p>
+        <input type="password" v-model="newUserParams.password_confirmation" />
+        <p></p>
+      </div>
+      <input type="submit" value="Submit" />
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.signup {
+  text-align: center;
+}
+</style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      newUserParams: {},
+      errors: [],
+    };
+  },
+  methods: {
+    submit: function () {
+      axios
+        .post("/users", this.newUserParams)
+        .then((response) => {
+          console.log(response.data);
+          this.$router.push("/login");
+        })
+        .catch((error) => {
+          this.errors = error.response.data.errors;
+        });
+    },
+  },
+};
+</script>
