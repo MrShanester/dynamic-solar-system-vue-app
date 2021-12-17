35f3b629426f4eb92e804112bb95e552e944a16c
Shane Nicholson <shanescottnicholson@gmail.com>
2021-12-17 13:39:00 -0800

    makes system show pretty

--- src/views/SystemShow.vue
+++ src/views/SystemShow.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="systemShow">
     <h1>{{ system.name }}</h1>
-    <img v-bind:src="system.image" v-bind:alt="`No Image`" class="rounded" />
+    <br />
+    <img v-bind:src="system.image" v-bind:alt="`No Image`" class="nice" />
     <p></p>
     <p></p>
     <h2>{{ system.description }}</h2>
@@ -12,7 +13,11 @@
       <div class="container" data-aos="fade-up">
         <div class="section-title">
           <h2>{{ system.name }}'s Planets</h2>
-          <p>View or Edit Planets</p>
+          <p class="name">
+            View or
+            <span class="span">Edit</span>
+            Planets
+          </p>
         </div>
         <div v-for="planet in planets" v-bind:key="planet.id">
           <router-link v-bind:to="`/planets/${planet.id}`">
@@ -34,7 +39,7 @@
                     <div class="row">
                       <div class="col-lg-8 details order-2 order-lg-1">
                         <router-link v-bind:to="`/planets/${planet.id}`">
-                          <h5 class="card-title">{{ planet.name }}</h5>
+                          <h5 class="special">{{ planet.name }}</h5>
                         </router-link>
                         <p class="fst-italic">
                           {{ planet.description }}
@@ -78,6 +83,45 @@
 </template>
 
 <style scoped>
+h1 {
+  font-family: "Poppins", sans-serif;
+  text-align: center;
+}
+
+.nice {
+  border: 2px solid #645ad4;
+  max-width: 100%;
+  height: auto;
+}
+
+h2 {
+  font-family: "Poppins", sans-serif;
+  text-align: center;
+  margin-left: 50px;
+}
+
+.special {
+  font-family: "Poppins", sans-serif;
+  text-align: center;
+  color: #645ad4;
+}
+
+.special:hover {
+  font-family: "Poppins", sans-serif;
+  text-align: center;
+  color: #b3afe8;
+}
+
+.name {
+  font-family: "Poppins", sans-serif;
+  color: white;
+}
+
+.span {
+  font-family: "Poppins", sans-serif;
+  color: #645ad4;
+}
+
 .systemShow {
   margin-top: 125px;
   text-align: center;
