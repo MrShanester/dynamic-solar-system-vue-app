3362316bf37c37a896bb83f8965503d75338cd28
Shane Nicholson <shanescottnicholson@gmail.com>
2021-12-21 14:08:34 -0800

    adds description box to planet show

--- src/views/PlanetShow.vue
+++ src/views/PlanetShow.vue
@@ -5,17 +5,18 @@
     <img class="nice" v-bind:src="planet.image" v-bind:alt="`No Image`" />
     <p></p>
     <h2>{{ planet.name }}'s Features:</h2>
-    <small>Diameter: {{ planet.diameter }} KM</small>
     <br />
-    <small>Mass: {{ planet.mass }} x 10^24 KG</small>
+    <p>Diameter: {{ planet.diameter }} KM</p>
+    <p></p>
+    <p>Mass: {{ planet.mass }} x 10^24 KG</p>
+    <p></p>
+    <p>Orbital Distance: {{ planet.orbital_distance }} AU</p>
+    <p></p>
+    <p>Orbital Period: {{ planet.orbital_period }} Standard Days</p>
     <br />
-    <small>Orbital Distance: {{ planet.orbital_distance }} AU</small>
-    <br />
-    <small>Orbital Period: {{ planet.orbital_period }} Standard Days</small>
-    <br />
-    <small>Description:</small>
-    <br />
-    <small>{{ planet.description }}</small>
+    <h2>Description:</h2>
+    <p></p>
+    <p class="desc">{{ planet.description }}</p>
     <p></p>
     <br />
     <router-link v-bind:to="`/planets/${planet.id}/edit`">
@@ -46,7 +47,7 @@ h2 {
   text-align: center;
 }
 
-small {
+p {
   font-family: "Poppins", sans-serif;
   text-align: center;
 }
@@ -55,6 +56,14 @@ small {
   border: 2px solid #645ad4;
   max-width: 100%;
   height: auto;
+}
+
+.desc {
+  border: 2px solid #525066;
+  margin-right: 150px;
+  margin-left: 150px;
+  padding: 12px 20px;
+  color: rgb(206, 204, 204);
 }
 </style>
 
